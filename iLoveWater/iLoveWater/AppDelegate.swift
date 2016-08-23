62ff2ebab2b20c2d58c9a9874c18693f8c61d0a5
yexiaohui <yexiaohui@yexiaohuis-MacBook-Pro.local>
2016-08-23 16:49:20 +0800

    新建项目

---
+++ iLoveWater/iLoveWater/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iLoveWater
+//
+//  Created by yexiaohui on 16/8/23.
+//  Copyright © 2016年 yexiaohui. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
